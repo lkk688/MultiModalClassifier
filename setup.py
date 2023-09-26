8021c18b0857412595fd3f300a330857661000af
vaibhavingale-sjsu <113152360+vaibhavingale-sjsu@users.noreply.github.com>
2023-09-26 12:26:27 -0700

    Update setup.py
    corrected package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
-requirements = ["numpy", "tensorflow", "PIL", "matplotlib"]
+requirements = ["numpy", "tensorflow", "pillow", "matplotlib"]
 
 setup(
     name="MultimodalClassifier",
